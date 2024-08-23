96bd172beff3cccbef1925cb3640b6b312f11fb2
John <johnsuyang2118@gmail.com>
2024-08-23 19:53:54 +0800

    added another listener function for edit and delete buttons

--- src/displayListeners.js
+++ src/displayListeners.js
@@ -1,4 +1,5 @@
 import dataPrepareUtil from "./dataPrepareUtil";
+import { projectDB, todoDB } from "./database";
 
 class DisplayListeners {
     constructor() {
@@ -7,11 +8,25 @@ class DisplayListeners {
 
     addListener({ selector, func, objectToBind, data }) {
         const element = document.querySelector(selector);
-        // I am binding the Project/Todo object inside the index.js here
-        // because the functionality will be coming from a different module
-        // which is being imported too in the index.js
-        data = this.dataPrepareUtil.prepare(data);
-        element.addEventListener("click", func.bind(objectToBind, data));
+        if (element) {
+            // I am binding the Project/Todo object inside the index.js here
+            // because the functionality will be coming from a different module
+            // which is being imported too in the index.js
+            data = this.dataPrepareUtil.prepare(data);
+            element.addEventListener("click", func.bind(objectToBind, data));
+        }
+    }
+
+    // for the edit and delete buttons listeners
+    addFormListeners({ selector, func, objectToBind, isEdit = false, collection }) {
+        const elements = document.querySelectorAll(selector);
+        if (elements.length) {
+            elements.forEach((elem) => {
+                const parent = elem.parentElement;
+                let data = this.dataPrepareUtil.getDataFromChildElements(parent, collection, isEdit);
+                elem.addEventListener("click", func.bind(objectToBind, data));
+            });
+        }
     }
 }
 
