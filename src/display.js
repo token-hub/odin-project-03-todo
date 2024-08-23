9a8596e9d428dbd4b7d83677b00049ea8f08e975
John <johnsuyang2118@gmail.com>
2024-08-23 18:04:09 +0800

    added some helper functions to Display

--- src/display.js
+++ src/display.js
@@ -1,7 +1,13 @@
 class Display {
     expandDetails() {}
-    getElementValue(selector) {
+    getElement(selector) {
         const element = document.querySelector(selector);
+        if (element) {
+            return element;
+        }
+    }
+    getElementValue(selector) {
+        const element = this.getElement(selector);
         if (element) {
             return element.value;
         }
@@ -21,6 +27,26 @@ class Display {
             dueDate,
             project
         };
+    }
+    displayProjects(data) {}
+    displayTodos(data) {
+        const element = this.getElement("#projects");
+
+        if (element) {
+            data.forEach((d) => {
+                const todo = document.createElement("div");
+                todo.id = d.id;
+                todo.classList.add("todo");
+                todo.innerHTML = `
+                    <p>${d.project}</p>
+                    <p>${d.title}</p>
+                    <p>${d.dueDate}</p>
+                    <p>${d.priority}</p>
+                    <p>${d.description}</p>`;
+
+                element.appendChild(todo);
+            });
+        }
     }
 }
 
