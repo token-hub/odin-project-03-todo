1119255dd45a8ac25d7863104cdabc4ebc365026
John <johnsuyang2118@gmail.com>
2024-08-23 19:53:03 +0800

    added a utility function to get the data from the children of a parentElement

--- src/dataPrepareUtil.js
+++ src/dataPrepareUtil.js
@@ -8,5 +8,27 @@ class DataPrepareUtil {
         delete data.generateId;
         return data;
     }
+
+    getDataFromChildElements(parentElement, collection, isEdit) {
+        let processedData = {};
+
+        if (!isEdit) {
+            processedData.id = parentElement.id;
+        } else {
+            if (collection === projectDB) {
+                processedData.id = parentElement.id;
+                processedData.projectName = parentElement.querySelector(".project-name");
+            } else if (collection === todoDB) {
+                processedData.id = parentElement.id;
+                processedData.projectId = parentElement.querySelector("todo-projectId");
+                processedData.title = parentElement.querySelector("todo-title");
+                processedData.dueDate = parentElement.querySelector("todo-dueDate");
+                processedData.priority = parentElement.querySelector("todo-priority");
+                processedData.description = parentElement.querySelector("todo-description");
+            }
+        }
+
+        return processedData;
+    }
 }
 export default DataPrepareUtil;
