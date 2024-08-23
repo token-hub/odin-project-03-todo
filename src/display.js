895483d2078246f571f77d9c495f2adce45652ca
John <johnsuyang2118@gmail.com>
2024-08-23 17:08:03 +0800

    Added a function to get form values

--- src/display.js
+++ src/display.js
@@ -1,3 +1,27 @@
 class Display {
     expandDetails() {}
+    getElementValue(selector) {
+        const element = document.querySelector(selector);
+        if (element) {
+            return element.value;
+        }
+        return null;
+    }
+    getFormValues() {
+        const title = this.getElementValue("#title");
+        const description = this.getElementValue("#description");
+        const priority = this.getElementValue("#priority");
+        const dueDate = this.getElementValue("#dueDate");
+        const project = this.getElementValue("#project");
+
+        return {
+            title,
+            description,
+            priority,
+            dueDate,
+            project
+        };
+    }
 }
+
+export default Display;
