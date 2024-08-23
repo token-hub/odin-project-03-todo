1091bc2f0218de74eed3cab0decdaf22eb4981c2
John <johnsuyang2118@gmail.com>
2024-08-23 17:08:35 +0800

    Add data as part of the bind when adding function to listeners

--- src/displayListeners.js
+++ src/displayListeners.js
@@ -1,12 +1,12 @@
 class DisplayListeners {
     constructor() {}
 
-    addListerner({ selector, func, objectToBind }) {
+    addListener({ selector, func, objectToBind, data }) {
         const element = document.querySelector(selector);
-        // I am binding the Main object inside the index.js here
+        // I am binding the Project/Todo object inside the index.js here
         // because the functionality will be coming from a different module
         // which is being imported too in the index.js
-        element.addEventListener("click", func.bind(objectToBind));
+        element.addEventListener("click", func.bind(objectToBind, data));
     }
 }
 
