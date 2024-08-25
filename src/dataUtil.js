ef113cb5f79c849f9c043e646ae0971b6ed838a8
John <johnsuyang2118@gmail.com>
2024-08-25 20:03:36 +0800

    remove getDataFromChildElements

--- src/dataUtil.js
+++ src/dataUtil.js
@@ -1,31 +1,8 @@
 import { v4 as uuidv4 } from "uuid";
-import { projectDB, todoDB } from "./storage/database";
 
 class DataUtil {
     generateId() {
         return uuidv4();
-    }
-
-    getDataFromChildElements(parentElement, collection, isEdit) {
-        let processedData = {};
-
-        if (!isEdit) {
-            processedData.id = parentElement.id;
-        } else {
-            if (collection === projectDB) {
-                processedData.id = parentElement.id;
-                processedData.projectName = parentElement.querySelector("p.project-name").textContent;
-            } else if (collection === todoDB) {
-                processedData.id = parentElement.id;
-                processedData.projectId = parentElement.querySelector("p.todo-projectId").textContent;
-                processedData.title = parentElement.querySelector("p.todo-title").textContent;
-                processedData.dueDate = parentElement.querySelector("p.todo-dueDate").textContent;
-                processedData.priority = parentElement.querySelector("p.todo-priority").textContent;
-                processedData.description = parentElement.querySelector("p.todo-description").textContent;
-            }
-        }
-
-        return processedData;
     }
 }
 export default DataUtil;
